aff213542fa6337cc39d241f0d03402c87749fdd
harshil <harshilsuthar427@gmail.com>
2024-08-02 19:39:36 +0530

    committed

---
+++ lib/config/api_keys.dart
@@ -0,0 +1,2 @@
+const String apiKey = "AIzaSyAN20t7qlDzfAC6uzIGYHTUMX7T970Q6nE";
+const String contextKey = "77bd5aae0b5e5433d";
---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:google_clone/colors.dart';
+import 'package:google_clone/responsive/mobile_screen_layout.dart';
+import 'package:google_clone/responsive/responsive_layout.dart';
+import 'package:google_clone/responsive/web_screen_layout.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Google Clone',
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: backgroundColor,
+      ),
+      home: const ResponsiveLayout(
+        mobileScreenLayout: MobileScreenLayout(),
+        webScreenLayout: WebScreenLayout(),
+      ),
+    );
+  }
+}
